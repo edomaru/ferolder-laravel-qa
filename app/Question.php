15b0771819e17462e06948bf5d5b612bd5a374b5
Fernandes Ferreira <ferolder@gmail.com>
2020-01-28 16:02:24 -0300

    Generate Question model and migration via artisan generator/ Gerando model e migration via artisan

---
+++ app/Question.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Illuminate\Support\Str;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    protected $fillable = ['title', 'body'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+/*   For use this relationship, do like below. 
+
+    $question = Question::find(1);
+    // To Access 
+    //$question->user->name;
+*/
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+    
+}
