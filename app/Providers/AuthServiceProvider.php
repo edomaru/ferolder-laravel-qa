abed0b00d7b1072d8349360cc72c2ac013ece250
ferolder <ferolder@gmail.com>
2020-02-04 15:02:32 -0300

    Implement authorization using Gates / Implementando autorização usando Gates

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,9 @@
 
 namespace App\Providers;
 
-use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Contracts\Auth\Access\Gate as AccessGate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,12 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('update-question', function($user, $question) {
+            return $user->id == $question->user_id;
+        });
+
+        Gate::define('delete-question', function ($user, $question) {
+            return $user->id == $question->user_id;
+        });
     }
 }
