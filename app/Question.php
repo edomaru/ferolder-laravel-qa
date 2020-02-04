190bc27cfe15754bcaf957dba016403e37549bae
ferolder <ferolder@gmail.com>
2020-02-04 16:59:39 -0300

    Implement authorization using Policy and fix data for questions / Implementando autorização usando Policy e consertando os dados das questions

--- app/Question.php
+++ app/Question.php
@@ -39,6 +39,17 @@ class Question extends Model
         return $this->created_at->diffForHumans();
     }
 
+    public function getStatusAttribute()
+    {
+        if ($this->answers > 0) {
+            if ($this->best_answer_id) {
+                return 'answered-accepted';
+            }
+            return 'answered';
+        }
+        return 'unanswered';
+    }
+
     public function getBodyHtmlAttribute()
     {
         return \Parsedown::instance()->text($this->body);
